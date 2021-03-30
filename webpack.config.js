50c908c772e15c64035ad2df7a81eb4371576ecb
paezjmv <paezjm86@gmail.com>
2021-03-29 21:46:16 -0600

    Añadiendo Babel y librerias relacionadas

--- webpack.config.js
+++ webpack.config.js
@@ -1,12 +1,34 @@
 const path = require('path');
+// path es un paquete que ya viene instalado con node
 
 module.exports = {
+  // Nos permite definir el punto de entrada de nuestra app
   entry: './src/index.js',
+  // Nos permite definir hacia donde se enviara lo preparado por webpack
   output: { 
+    /* Path es donde estara la carpeta donde se guardaran 
+    los archivos */
+    /* Path.resolve nos permite defnir la ruta especifica. 
+    Gracias a __dirname rompemos la ambiguedad de que se 
+    ubique en rutas especificas. Este siempre creara una 
+    carpeta del nombre 'dist' */
     path: path.resolve(__dirname, 'dist'),
+    // Este nombrar al archivo final
     filename: 'main.js',
    },
    resolve: {
+    //  Aqui ponemos las extensiones con las que webpack trabajara
      extensions: ['.js']
-   }
+   },
+   module: {
+     rules: [
+        {
+          test: /\.m?js$/,
+          exclude: /node_modules/,
+          use: {
+            loader: 'babel-loader'
+        }
+      }
+    ]
+  }
 }
