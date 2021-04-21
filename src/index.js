32ea9164becc7a8d0dd700b3d65109528e5f5307
paezjmv <paezjm86@gmail.com>
2021-04-21 14:18:14 -0500

    Instalando React al proyecto

--- src/index.js
+++ src/index.js
@@ -1,7 +1,14 @@
-import Template from '@templates/Template.js';
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './components/App.jsx';
+
+ReactDOM.render(<App />, document.getElementById('app'));
+
+/* import Template from '@templates/Template.js';
 import '@styles/main.css';
 
 (async function App() {
   const main = null || document.getElementById('main');
   main.innerHTML = await Template();
 })();
+ */
